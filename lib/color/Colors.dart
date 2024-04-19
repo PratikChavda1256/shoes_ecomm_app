8b21bceb7acbde9d1335e15f6727504402978088
PratikChavda1256 <91555057+PratikChavda1256@users.noreply.github.com>
2024-04-19 17:39:02 +0530

    Delete lib directory

--- lib/color/Colors.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-class ShoesColors {
-  static const Color loginBg = Color(0xffF8F9FA);
-  static const Color textBg = Color(0xffffffff);
-  static const Color loginButtonColor = Color(0xff5B9EE1);
-
-
-}
--- lib/controller/ControllerSupport.dart
+++
@@ -1,5 +0,0 @@
-/*
-*
-* Controller support file
-*
-* */
